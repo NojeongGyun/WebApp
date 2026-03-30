b5f1dbc651214567902a6e0138a0fb9a4fe3e27b
NojeongGyun <shwjdrbs12@gmail.com>
2026-03-30 21:28:52 +0900

    Update vite.config.ts

--- 4주차(0325)/upload/vite.config.ts
+++ 4주차(0325)/upload/vite.config.ts
@@ -1,22 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+
+  base: '/WebApp/4주차(0325)/upload/',
+  
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
   },
-}));
+});
