33cbd2b8716e986dc9934b37bf36c260774e4fad
NojeongGyun <shwjdrbs12@gmail.com>
2026-04-01 10:12:55 +0900

    Add files via upload

---
+++ 5주차(0401)/upload/comp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import Header from './funcComp/Header';
+import Footer from './funcComp/Footer';
+import Main from './funcComp/Main';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+export default App;
